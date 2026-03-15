0642ef87eaba085073e2e37b82a89cf185a715c5
Ville Vartio <ville.vartio@metropolia.fi>
2026-03-15 21:42:05 +0200

    .

--- Week 2/ex_6.c
+++ Week 2/ex_6.c
@@ -3,28 +3,43 @@
 #include <time.h>
 #include <stdbool.h>
 
-int main (void) {
-    srand (time(NULL));
-    int d6 = rand() % 6;
-    int d10 = rand () %10;
+void roll_d6(void) {
+    int result = rand() % 6 + 1;
+    printf("Your d6 landed on: %d\n", result);
+}
+
+void roll_d10(void) {
+    int result = rand() % 10 + 1;
+    printf("Your d10 landed on: %d\n", result);
+}
+
+int main(void) {
+    srand(time(NULL));
     int choice = 0;
-    int quit = false;
-    while (quit != true) {
-        printf("Choose your action;\n");
-        printf("Roll a d6\n"
-           "Roll a d10\n"
-           "Quit\n");
-        scanf("%d", &choice);
-        if (choice == 3) {
-            quit = true;
+
+    while (true) {
+        printf("\nChoose your action:\n");
+        printf("1) Roll a D6\n");
+        printf("2) Roll a D10\n");
+        printf("3) Quit\n");
+
+        if (scanf("%d", &choice) != 1) {
+            while (getchar() != '\n');
+            printf("NO! bad input! Enter 1, 2, or 3.\n");
+            continue;
         }
-        else if (choice == 1) {
-            printf("your d6 landed on:&d6");
-            choice = 0;
-        }
-        else if (choice == 2) {
-            printf("your d10 landed on:&d10");
-            choice = 0;
+
+        if (choice == 1) {
+            roll_d6();
+        } else if (choice == 2) {
+            roll_d10();
+        } else if (choice == 3) {
+            printf("Peace!\n");
+            break;
+        } else {
+            printf("NO! bad input! Enter 1, 2, or 3.\n");
         }
     }
+
+    return 0;
 }
